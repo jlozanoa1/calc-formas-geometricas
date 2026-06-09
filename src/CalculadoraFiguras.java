532a8e4a29b3c6155da6b7e01cb1730b035d9a40
Juan Lozano <jlozanoa1@unicartagena.edu.co>
2026-06-09 12:33:52 -0500

    Creación de proyecto base en Java. Presentación inicial del menú de la aplicación.

---
+++ src/CalculadoraFiguras.java
@@ -0,0 +1,13 @@
+public class CalculadoraFiguras {
+    // Parametros: lado | Retorna: area -> double
+    public static double calcularAreaCuadrado() {}
+
+    // Parametros: ancho, alto | Retorna: area -> double
+    public static double calcularAreaRectangulo() {}
+
+    // Parametros: base, altura | Retorna: area -> double
+    public static double calcularAreaTriangulo() {}
+
+    // Parametros: radio | Retorna: area -> double
+    public static double calcularAreaCirculo() {}
+}
---
+++ src/CalculadoraFigurasCli.java
@@ -0,0 +1,11 @@
+public class CalculadoraFigurasCli {
+    public static void inicializar () {
+        System.out.println("=== Calculadora de Figuras Geométricas ===");
+        System.out.println("1. Área de cuadrado.");
+        System.out.println("2. Área de rectángulo.");
+        System.out.println("3. Área de triángulo.");
+        System.out.println("4. Área de círculo.");
+        System.out.println("5. Salir.");
+        System.out.printf("%nSeleccione una opción: ");
+    }
+}
